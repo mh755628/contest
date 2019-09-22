9cc8224e83db834faf3790e50ad8dfa6f989d189
mh755628 <mh755628@gmail.com>
2019-09-22 19:46:20 +0600

    Update segmenttree.cpp

--- segmenttree.cpp
+++ segmenttree.cpp
@@ -14,7 +14,7 @@ struct RMQ {
                 for(t[p += n] = v; p > 1; p >>= 1) t[p >> 1] = min(t[p], t[p ^ 1]);
         }
         int query(int l, int r) { int res = inf;
-                for(l += n, r += n; l < r; l >>= 1, r >>= 1) {
+                for(l += n, r += n + 1; l < r; l >>= 1, r >>= 1) {
                         if(l & 1) res = min(res, t[l++]);
                         if(r & 1) res = min(res, t[--r]);
                 }
