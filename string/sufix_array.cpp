f8a136f98ab6666504a70ec8f6f159b600be91ba
mh755628 <mh755628@gmail.com>
2020-08-20 01:18:22 +0600

    Update sufix_array.cpp

--- string/sufix_array.cpp
+++ string/sufix_array.cpp
@@ -1,5 +1,7 @@
+
 struct SA{
 	int n; vector <int> lcp, sa, rank;
+	vector <vector <int> > t;
 	SA() {}
 	SA(string str) : n(str.size()) {
 		vector <int> p(n), c(n), cnt(max(1 << 8, n), 0);
@@ -34,4 +36,36 @@ struct SA{
 			if(k) --k;
 		}
 	}
-};
+	void build_rmq() {
+		int l = 32 - __builtin_clz(n);
+		t = vector <vector <int> > (l, vector <int> (n, 0));
+		for(int i = 0; i < n; i++) t[0][i] = lcp[i];
+		for(int i = 1; i < l; i++) {
+			for(int j = 0; j + (1 << i) - 1 < n; j++) {
+				t[i][j] = min(t[i - 1][j], t[i - 1][j + (1 << (i - 1))]);
+			}
+		}
+	}
+	int query(int l, int r) {
+		int h = 31 - __builtin_clz(r - l + 1);
+		return min(t[h][l], t[h][r - (1 << h) + 1]);
+	}
+	int find_left(int i, int k) {
+		int l = 1, r = rank[i] - 1, j = rank[i];
+		while(l <= r) {
+			int mid = (l + r) >> 1;
+			if(query(mid, rank[i] - 1) >= k) j = mid, r = mid - 1;
+			else l = mid + 1;
+		}
+		return j;
+	}
+	int find_right(int i, int k) {
+		int l = rank[i] + 1, r = n - 1, j = rank[i];
+		while(l <= r) {
+			int mid = (l + r) >> 1;
+			if(query(rank[i], mid - 1) >= k) j = mid, l = mid + 1;
+			else r = mid - 1;
+		}
+		return j;
+	}
+}sa;
