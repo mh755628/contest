52c8eee5b48984e70c0e14e28ac50cce638e9757
mh755628 <mh755628@gmail.com>
2020-10-28 16:34:37 +0600

    Update Aho-Corasick.cpp

--- string/Aho-Corasick.cpp
+++ string/Aho-Corasick.cpp
@@ -1,51 +1,60 @@
 struct AC {
-    static const int K = 1 << 8;
+    static const int K = 26;
     struct node {
-        int nxt[K], go[K], link, leaf, par; 
+        int nxt[K], link, leaf, par; 
         char pch;
         node(int par = -1, char pch = '$') : par(par), pch(pch) {
             fill(begin(nxt), end(nxt), -1);
-            fill(begin(go), end(go), -1);
             leaf = false;
             link = -1;
         }
     };
-    vector <node> tree;
+    vector <node> t;
+    vector <vector <int> > ad;
+    vector <int> st, en, mp;
+    int dt = 0;
     AC() {
-        tree.resize(1);
+        mp = vector <int> (1 << 8, 0);
+        for(char ch = 'a'; ch <= 'z'; ++ch) mp[ch] = ch - 'a';
+        t.resize(1);
     }
     int add(string str) {
         int ptr = 0;
         for(auto ch: str) {
-            if(tree[ptr].nxt[ch] == -1) {
-                tree[ptr].nxt[ch] = tree.size();
-                tree.push_back(node(ptr, ch));
+            if(t[ptr].nxt[mp[ch]] < 0) {
+                t[ptr].nxt[mp[ch]] = t.size();
+                t.push_back(node(ptr, ch));
             }
-            ptr = tree[ptr].nxt[ch];
+            ptr = t[ptr].nxt[mp[ch]];
         }
-        tree[ptr].leaf = true;
+        t[ptr].leaf = true;
         return ptr;
     }
-
     int get_link(int v) {
-        if(tree[v].link == -1) {
-            if(v == 0 || tree[v].par == 0) {
-                tree[v].link = 0;
-            } else {
-                tree[v].link = go(get_link(tree[v].par), tree[v].pch);
-            }
+        if(t[v].link == -1) {
+            t[v].link = (!v || !t[v].par) ? 0 : go(get_link(t[v].par), t[v].pch);
         }
-        return tree[v].link;
+        return t[v].link;
     }
     int go(int v, char ch) {
-        if(tree[v].go[ch] == -1) {
-            if(tree[v].nxt[ch] != -1) {
-                tree[v].go[ch] = tree[v].nxt[ch];
-            } else {
-                tree[v].go[ch] = (v == 0) ? 0 : go(get_link(v), ch);
-            }
+        if(t[v].nxt[mp[ch]] < 0) {
+            t[v].nxt[mp[ch]] = !v ? 0 : go(get_link(v), ch);
         }
-        return tree[v].go[ch];
+        return t[v].nxt[mp[ch]];
     }
-
-};
+    void dfs(int v) {
+        st[v] = ++dt;
+        for(int u: ad[v]) 
+            dfs(u);
+        en[v] = dt;
+    }
+    void calc() {
+        int k = t.size();
+        st.resize(k);
+        en.resize(k);
+        ad.resize(k);
+        for(int i = 1; i < k; i++) ad[get_link(i)].push_back(i);
+        dfs(0);
+    }
+ 
+}ac;
