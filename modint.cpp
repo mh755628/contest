f3820e7df62d80711cb4f49d152f92c50d8868e4
mh755628 <mh755628@gmail.com>
2020-12-05 15:55:18 +0600

    Update modint.cpp

--- modint.cpp
+++ modint.cpp
@@ -1,21 +1,23 @@
 template <int mod = 1000000007>
 struct modint{
     int x;
-    modint(int y = 0) : x(y % mod) { x = x < 0 ? x + mod : x;}
-    modint& operator+=(modint const& b) {x += b.x; x = (x >= mod) ? x - mod : x;return *this;}
-    modint& operator-=(modint const& b) {x = x >= b.x ? x - b.x : x - b.x + mod;return *this;}
-    modint& operator*=(modint const& b) {x = 1LL * x * b.x % mod;return *this;}
-    friend ostream& operator<<(ostream& os, modint const& a) {return os << a.x;}
+    modint(int y = 0) : x((y % mod + mod) % mod) { }
     friend modint operator^(modint a, long long b) {
-        modint res = 1;
-        for(int i = b; i; i >>= 1, a *= a) if(i & 1) res *= a;
-        return res;
+        modint r = 1;
+        for(; b; b >>= 1, a *= a) if(b & 1) r *= a;
+        return r;
     }
-    friend modint operator!(modint a) {return (a ^ (mod - 2));}
-    modint& operator/=(modint const& b) {return *this *= (!b);}
+    friend modint operator-(modint a) {return modint(0) - a;}
+    friend modint operator!(modint a) {return a ^ (mod - 2);}
+    modint& operator/=(modint const& b) {return *this *= !b;}
     friend modint operator+(modint a, modint const& b) {return a += b;}
     friend modint operator-(modint a, modint const& b) {return a -= b;}
     friend modint operator*(modint a, modint const& b) {return a *= b;}
     friend modint operator/(modint a, modint const& b) {return a /= b;}
-    friend modint operator-(modint a) {return 0 - a;}
+    modint& operator*=(modint const& b) {x = 1LL * x * b.x % mod; return *this;}
+    friend ostream& operator<<(ostream& os, modint const& a) {return os << a.x;}
+    modint& operator+=(modint const& b) {x += b.x; x = (x >= mod) ? x - mod : x;return *this;}
+    modint& operator-=(modint const& b) {x = x >= b.x ? x - b.x : x - b.x + mod;return *this;}
 };
+
+using mint = modint <998244353>;
