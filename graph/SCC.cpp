7869ac492da7ab12de8bc36d4f0071420b781e66
mh755628 <mh755628@gmail.com>
2023-11-28 00:20:25 +0600

    Update SCC.cpp

--- graph/SCC.cpp
+++ graph/SCC.cpp
@@ -1,47 +1,68 @@
 struct SCC{
-    const static int N = 2000 + 7;
-
-    vector <int> adj[N], r_adj[N];
-    int V, visit[N], cnt, color[N];
+    int n, componentCount = 0;
+    vector <vector <int>> adjacencyList, reverseAdjacencyList;
+    vector <int> isVisited, componentId;
     stack <int> stk;
-
-    SCC(int V) : V(V), cnt(0) {
-        memset(visit, 0, sizeof visit);
+ 
+    SCC(int n) : n(n) {
+        adjacencyList = reverseAdjacencyList = vector <vector <int>> (n + 1);
+        isVisited = componentId = vector <int> (n + 1, 0);
     }
-
-    void add_edge(int x, int y) {
-        adj[x].push_back(y);
-        r_adj[y].push_back(x);
+ 
+    void addEdge(int u, int v) {
+        adjacencyList[u].push_back(v);
+        reverseAdjacencyList[v].push_back(u);
     }
-
+ 
     void dfs(int u) { 
-        visit[u] = true;
-        for(int v: adj[u]) if(!visit[v]) {
+        isVisited[u] = true;
+        for(int v: adjacencyList[u]) if(!isVisited[v]) {
             dfs(v);
         }
         stk.push(u);
     }
-
+ 
     void dfs(int u, int id) {
-        visit[u] = false; color[u] = id;
-        for(int v: r_adj[u]) if(visit[v]) {
+        isVisited[u] = false; 
+        componentId[u] = id;
+        for(int v: reverseAdjacencyList[u]) if(isVisited[v]) {
             dfs(v, id);
         }
-    }
-
-
-    int get() {
-        for(int i = 1; i <= V; i++) if(!visit[i]) {
-            dfs(i);
+    } 
+ 
+    int init() {
+        for(int i = 1; i <= n; i++) { 
+            if(!isVisited[i]) {
+                dfs(i);
+            }
         }
-        while(stk.size()) {
-            int v = stk.top(); stk.pop(); 
-            if(visit[v]) {
-                dfs(v, ++cnt);
+        while(not stk.empty()) {
+            int v = stk.top(); 
+            stk.pop(); 
+            if(isVisited[v]) {
+                dfs(v, ++componentCount);
             }
         } 
-        return cnt;
+        return componentCount;
     }
 
+    int getId(int u) {
+        return componentId[u];
+    }
+ 
+    int operator[](int v) {
+        return componentId[v];
+    }
 
+    vector <vector <int>> getCondenstaionGraph() {
+        vector <vector <int>> ret(n + 1);
+        for(int i = 1; i <= n; i++) {
+            for(int j: adjacencyList[i]) {
+                if(getId(i) ^ getId(j)) {
+                    ret[componentId[i]].push_back(componentId[j]);
+                }
+            }
+        }
+        return ret;
+    }
 };
